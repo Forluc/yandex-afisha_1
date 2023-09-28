6912589a26c0202c64c938db3e92b689a25def9f
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-09-28 20:49:57 +0300

    add preview images in django admin

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 from places.models import Place, Images
+from django.utils.html import format_html
+
+SIZE_IMAGE = 200
 
 
 @admin.register(Images)
-class AdminImages(admin.ModelAdmin):
-    raw_id_fields = ['place', ]
+class AdminImage(admin.ModelAdmin):
+    raw_id_fields = ['place']
+    readonly_fields = ['show_preview']
+
+    def show_preview(self, obj):
+        return format_html(
+            '<img src="{}" width="{}">', obj.image.url, 200
+        )
 
 
 class ImageInline(admin.TabularInline):
     model = Images
+    readonly_fields = ['show_preview']
+
+    def show_preview(self, obj):
+        return format_html(
+            '<img src="{}" width="{}">', obj.image.url, 200
+        )
 
 
 @admin.register(Place)
