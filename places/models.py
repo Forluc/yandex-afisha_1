02c9459ce9cc4160e2246389c2195c8a41bcd027
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-10-01 17:05:30 +0300

    add WYSIWYG-editor(django-tinymce)

--- places/models.py
+++ places/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(verbose_name='title', max_length=100)
     description_short = models.TextField(verbose_name='description_short')
-    description_long = models.TextField(verbose_name='description_long')
+    description_long = HTMLField(verbose_name='description_long')
     latitude = models.FloatField(verbose_name='latitude')
     longitude = models.FloatField(verbose_name='longitude')
 
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
     'places',
     'adminsortable2',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
